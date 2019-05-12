2b78dc9ee01332c6fe11dc98b9e6da1e0931f976
Elena <elena.shuba@ukr.net>
2019-05-12 14:07:39 +0300

    This is my first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+a = 5;
+console.log(a);
+
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+console.log(person['name']);
+
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = +prompt("Есть ли ва 18?", "Yes");
+//console.log(answer);
+
+//console.log(4 + + "-object");
+let incr = 10,
+decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log("2" ===2);
+
+let isChecked = false,
+    isClose = false;
+    console.log(isChecked || !isClose);
+
